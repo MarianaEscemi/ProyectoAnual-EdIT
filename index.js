af4b96e9c5b02f2ce5c27a8105396333912f1c2b
Marian <mariana.escemi@gmail.com>
2022-12-14 22:16:08 -0300

    Actualice deploy

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 //Creamos constantes y requerimos las liblerias
 const express = require('express');
 const path = require('path');
@@ -9,7 +11,6 @@ const hbs = require('hbs');
 const PORT = 3000;
 const app = express();
 
-
 // Middlewares Funciones para ayudar a apuntar a diferentes directorios:: entender json
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,13 +18,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Configuraciones se utilizan con set y van antes de las rutas
 //conf handelbars
 
-
-
-
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
-
 
 //Rutas
 app.get('/', (req, res) =>{
@@ -35,15 +32,15 @@ app.get('/nosotros', (req, res) =>{
 app.get('/especialidad', (req, res) =>{
     res.render('especialidad');
 }); 
-app.get('/revestimientos', (req, res) =>{
+/* app.get('/revestimientos', (req, res) =>{
     res.render('revestimientos');
-}); 
+});  */
 app.get('/disenos', (req, res) =>{
     res.render('disenos');
 }); 
-app.get('/ideas', (req, res) =>{
+/* app.get('/ideas', (req, res) =>{
     res.render('ideas');
-}); 
+});  */
 app.get('/productos', (req, res) =>{
     res.render('productos');
 });
@@ -51,19 +48,13 @@ app.get('/contacto', (req, res) =>{
     res.render('contacto');
 });
 
-app.get('/stuccoveneciano', (req, res) =>{
-    res.render('stuccoveneciano');
-});
-
-
 
 //app en escucha por el puerto asignado
 app.listen(PORT, () => {
     console.log(`Aplicacion Activa y trabajando en el Puerto ${PORT}`);
 });
 
-
 // En caso de error me avisa
 app.on('error', () => {
-    console.log('tenemos un error en el espacio');
+    console.log('tenemos un error en la App');
 })
