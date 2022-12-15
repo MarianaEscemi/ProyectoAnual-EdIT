014c495499b229e1327cd836c08c29c6d8a02a58
Marian <mariana.escemi@gmail.com>
2022-12-15 14:21:04 -0300

    detalles para deploy

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 
 
 //Creamos constantes y requerimos las liblerias
-const express = require('express');
+/* const express = require('express');
 const path = require('path');
-const hbs = require('hbs');
-//const { join } = require('path');
+const hbs = require('hbs'); */
+import express from 'express';
+import path from 'path';
+import hbs from 'hbs';
+import morgan from 'morgan';
+import 'dotenv/config';
+import {fileURLToPath} from 'url';
+import './db/conexion.js';
+import { router } from './router/homeRouter.js'
 
 
 //Ejecutamos express y guardamos el puerto
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+//__dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Middlewares Funciones para ayudar a apuntar a diferentes directorios:: entender json
+app.use(morgan('common'));  //dev common combined
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 //Configuraciones se utilizan con set y van antes de las rutas
 //conf handelbars
@@ -22,8 +36,12 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
+//rutas
+app.use(router)
+
+
 //Rutas
-app.get('/', (req, res) =>{
+/* app.get('/', (req, res) =>{
     res.render('index');
 });
 app.get('/nosotros', (req, res) =>{
@@ -32,21 +50,25 @@ app.get('/nosotros', (req, res) =>{
 app.get('/especialidad', (req, res) =>{
     res.render('especialidad');
 }); 
-/* app.get('/revestimientos', (req, res) =>{
-    res.render('revestimientos');
-});  */
+
 app.get('/disenos', (req, res) =>{
     res.render('disenos');
 }); 
-/* app.get('/ideas', (req, res) =>{
-    res.render('ideas');
-});  */
+
 app.get('/productos', (req, res) =>{
     res.render('productos');
 });
 app.get('/contacto', (req, res) =>{
     res.render('contacto');
-});
+}); */
+
+
+/* app.get('/revestimientos', (req, res) =>{
+    res.render('revestimientos');
+});  */
+/* app.get('/ideas', (req, res) =>{
+    res.render('ideas');
+});  */
 
 
 //app en escucha por el puerto asignado
